9b19c8aaf286d99399de10a5d7515c9bb4f104de
vennylee <hs421@naver.com>
2022-07-04 15:12:53 +0900

    feat: 카카오 토큰 받아서 클라에 저장, 유저 정보 조회 성공

--- src/elements/introMain/KakaoLoginCallBack.js
+++ src/elements/introMain/KakaoLoginCallBack.js
@@ -6,48 +6,43 @@ import { setCookie } from "../../shared/cookie";
 const KakaoLoginCallback = () => {
   const location = useLocation();
 
-  const getNaverToken = () => {
+  const getKakaoToken = () => {
     const code = location.search.split("=")[1];
     console.log(code);
     // 인가 코드 서버로 전송
     axios
-      .post(`http://52.78.168.151:3000/oauth/callback/kakao`, {
+      .post(`http://52.78.168.151:3000/auth/login/kakao/callback`, {
         code: code,
       })
       .then((res) => {
-        console.log(res);
-        console.log(res.json());
-        // const token = res.data.access_token;
-        // console.log(token);
-        // setCookie(token);
-      });
+        const token = res.data;
+        setCookie(token);
+        axios
+          .post(`http://52.78.168.151:3000/kakao/member`, {
+            token,
+          })
+          .then((res) => {
+            const user_email = res.data.kakao_account.email;
+            const user_id = res.data.id;
+            const user_name = res.data.kakao_account.profile.nickname;
+            console.log(user_email, user_id, user_name); // 성공
+
+            axios
+              .post(`http://52.78.168.151:3000/kakao/parsing`, {
+                user_email,
+                user_id,
+                user_name,
+              })
+              .then((res) => console.log(res));
+          });
+      })
+      .catch((err) => console.log(err));
   };
 
-  // 받아온 토큰으로 유저 정보 조회
-  //   axios
-  //     .post(`http://52.78.168.151:3000/member`, {
-  //       token,
-  //     })
-  //     .then((res) => {
-  //       const user_email = res.data.response.email;
-  //       const user_id = res.data.response.id;
-  //       const user_name = res.data.response.name;
-
-  //       axios
-  //         .post(`http://52.78.168.151:3000/parsing`, {
-  //           user_email,
-  //           user_id,
-  //           user_name,
-  //         })
-  //         .then((res) => console.log(res));
-  //     })
-  //     .catch((error) => console.log(error));
-  // })
-  // .catch((err) => console.log(err));
-  // };
+  //   받아온 토큰으로 유저 정보 조회
 
   useEffect(() => {
-    getNaverToken();
+    getKakaoToken();
   }, []);
   return <div>KakaoLoginCallback페이지입니다</div>;
 };
