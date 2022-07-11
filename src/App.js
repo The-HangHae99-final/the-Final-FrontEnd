db5b92ad2513f6c342506660695e3e8754b56d43
vennylee <hs421@naver.com>
2022-07-11 14:23:34 +0900

    Feat: Maintain login status

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import NaverLoginCallBack from "./elements/introMain/NaverLoginCallBack";
 import KakaoLoginCallback from "./elements/introMain/KakaoLoginCallBack";
 import { getCookie } from "./shared/cookie";
-import { useSelector } from "react-redux";
-import Modal from "./components/Modal";
+import { useDispatch, useSelector } from "react-redux";
 
-// pages
+// pages & components
 import Main from "./pages/Main/Main";
 import Board from "./pages/Board";
 import Message from "./pages/Message/Message";
@@ -15,13 +14,15 @@ import Calender from "./pages/Calendar/Calendar";
 import Storage from "./pages/Storage";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Modal from "./components/Modal";
+import { login } from "./redux/userReducer";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
   const user = useSelector((state) => state.user.value);
-
+  const dispatch = useDispatch();
+  const isLoggedIn = user.isLoggedIn;
   const handleLogin = () => {
     setIsLoginModalOpen(!isLoginModalOpen);
     setIsSignupModalOpen(false);
@@ -31,15 +32,38 @@ const App = () => {
     setIsSignupModalOpen(!isSignupModalOpen);
     setIsLoginModalOpen(false);
   };
+
+  // loadUsser func.
+
+  const ls = localStorage.getItem("myToken");
+
+  // useEffect(() => {
+  //   const loadUser = () => {
+  //     try {
+
+  //       // user 값이 없다면 아무것도 리턴하지 않음
+  //       if (!user) return;
+
+  //       // user 값이 있다면 dispatch로 로그인 상태를 true로 업데이트
+  //       dispatch(login({ ...user, isLoggedIn: true }));
+  //       return <div>로그인 됬을 때 보여질 것들</div>
+  //     } catch (e) {
+  //       console.log("local storage is not working");
+  //     }
+  //   };
+  //   loadUser();
+  // }, []);
+
   console.log(user.isLoggedIn);
+
   return (
     <div>
       <GlobalStyle />
       <Routes>
-        {user.isLoggedIn ? (
+        {ls ? (
           <>
             <Route
-              path="/main"
+              path="/"
               element={
                 <Main
                   isLoggedIn={isLoggedIn}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import loginBg from "../public/img/Ellipse103.png";
+import Divider from "../elements/Divider";
+import SocialLogin from "./SocialLogin";
 
 const Signup = () => {
   const [signupValue, setSignupValue] = useState({
@@ -70,6 +73,114 @@ const Signup = () => {
   );
 };
 
-const SignupStyle = styled.div``;
+const SignupStyle = styled.div`
+  /* background: url("public/img/loginBg.png"); */
+  height: 100vh;
+  width: 100vw;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const LoginBackGround = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+  backdrop-filter: blur(10px);
+  position: fixed;
+  top: 0;
+  left: 0;
+  cursor: pointer;
+  text-align: center;
+`;
+
+const LoginStyle = styled.div`
+  padding: 50px 100px 81px 100px;
+  background: rgba(254, 254, 254, 0.5);
+  opacity: 0.9;
+  border-radius: 20px;
+  width: 560px;
+  height: 532px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const LoginTitle = styled.div`
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 60px;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: var(--point-main);
+  text-shadow: 0px 0px 20px rgba(192, 200, 248, 0.49);
+  margin-bottom: 37px;
+`;
+
+const EmailWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const EmailLabel = styled.div`
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 17px;
+  color: var(--main-grey);
+`;
+
+const EmailInput = styled.input`
+  all: unset;
+  border-bottom: var(--FEFEFE);
+  width: 100%;
+  height: 20px;
+  padding: 10px 10px 10px 0px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const EmailSubmit = styled.div`
+  width: 100px;
+  height: 40px;
+  background: rgba(247, 247, 247, 0.9);
+  border-radius: 5px;
+
+  cursor: pointer;
+  display: Flex;
+  justify-content: center;
+  align-items: center;
+  justify-self: flex-end;
+  margin-top: 19px;
+  margin-bottom: 64px;
+  color: var(--main-grey);
+  position: position;
+`;
+
+const LoginWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+const ContinueWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 25px;
+`;
+
+const ContinueText = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 24px;
+  color: #7a858e;
+  width: 400px;
+  margin: 0px 25px;
+`;
 
 export default Signup;
