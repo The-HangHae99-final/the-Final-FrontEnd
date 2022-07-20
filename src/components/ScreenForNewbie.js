5425f865c46fea85f16c8b623a173992229d68ec
vennylee <hs421@naver.com>
2022-07-20 21:13:30 +0900

    Style: make screen for newbie

---
+++ src/components/ScreenForNewbie.js
@@ -0,0 +1,146 @@
+import React from "react";
+import styled from "styled-components";
+import topArrow from "../public/img/top-arrow.png";
+
+const ScreenForNewbie = () => {
+  return (
+    <ScreenForNewbieStyle>
+      <div className="create-box-container">
+        <div className="create-box-wrap">
+          <h3 className="create-box_title">New Work Space</h3>
+          <div className="create-box_subtitle">
+            워크스페이스의 이름을 지어주세요.
+          </div>
+          <div className="input-wrap">
+            <input
+              type="text"
+              className="create-box_input"
+              placeholder="10글자 내외로 작성해주세요."
+            />
+            <div className="input-actions">
+              <span className="input-max-length">0/10</span>
+              <img src={topArrow} alt="topArrow" className="topArrow" />
+            </div>
+          </div>
+          <div className="add-later">나중에 할래요</div>
+        </div>
+      </div>
+    </ScreenForNewbieStyle>
+  );
+};
+
+const ScreenForNewbieStyle = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  left: 0;
+  top: 0;
+  text-align: center;
+  z-index: 999;
+  backdrop-filter: blur(10px);
+
+  .create-box-container {
+    width: 434px;
+    height: 227px;
+    background: #ffffff;
+    box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.1);
+    padding: 30px 44px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    .create-box-wrap {
+      width: 100%;
+    }
+  }
+
+  .create-box_title {
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 27px;
+    letter-spacing: -0.02em;
+    color: var(--point-main);
+  }
+
+  .create-box_subtitle {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.02em;
+    color: #7a858e;
+    margin-bottom: 28px;
+  }
+
+  .input-wrap {
+    display: flex;
+    margin-bottom: 25px;
+    align-items: center;
+    width: 100%;
+    position: relative;
+  }
+
+  .create-box_input {
+    border: none;
+    padding: 15px;
+    width: 100%;
+    background: #f8f8f9;
+    border-radius: 5px;
+    outline: none;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.02em;
+    color: #353841;
+
+    ::placeholder {
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 20px;
+      letter-spacing: -0.02em;
+      color: #cbcbd7;
+    }
+  }
+
+  .input-actions {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 60px;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: 0;
+    margin-right: 10px;
+  }
+
+  .input-max-length {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: right;
+    letter-spacing: -0.02em;
+    color: #7a858e;
+  }
+
+  .topArrow {
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+    margin-left: 10px;
+  }
+
+  .add-later {
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 17px;
+    letter-spacing: -0.02em;
+    text-decoration-line: underline;
+    color: #cbcbd7;
+    cursor: pointer;
+  }
+`;
+
+export default ScreenForNewbie;
