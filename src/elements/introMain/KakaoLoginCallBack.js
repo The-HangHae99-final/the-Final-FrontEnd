5bf39801cddf542d0378bfb5df8b0932347f774b
vennylee <hs421@naver.com>
2022-07-20 12:57:58 +0900

    Solve: kakao login issue

--- src/elements/introMain/KakaoLoginCallBack.js
+++ src/elements/introMain/KakaoLoginCallBack.js
@@ -1,40 +1,34 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
-import { setCookie } from "../../shared/cookie";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { login } from "../../redux/userReducer";
 import { setItemToLs } from "../../components/localStorage";
 
 const KakaoLoginCallback = () => {
-  const [userInfo, setUserInfo] = useState({
-    user_email: "",
-    user_id: "",
-    user_name: "",
-  });
   const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const getKakaoToken = () => {
     const code = location.search.split("=")[1];
+
     // 인가 코드 서버로 전송
     axios
       .post(`http://52.79.251.110:3001/api/auth/login/kakao/callback`, {
         code: code,
       })
-      .then((res) => {
-        const token = res.data;
-        console.log("token: ", token);
 
+      .then((res) => {
+        console.log(res);
+        const token = res.data;
         // 서버가 받아온 유저 정보 조회
         axios
           .post(`http://52.79.251.110:3001/api/kakao/member`, {
             token,
           })
           .then((res) => {
-            console.log(`member req : ${res}`);
             const user_email = res.data.kakao_account.email;
             const user_id = res.data.id;
             const user_name = res.data.kakao_account.profile.nickname;
@@ -48,10 +42,11 @@ const KakaoLoginCallback = () => {
                 user_name,
               })
               .then((res) => {
-                console.log(res);
                 setItemToLs("myToken", token);
+                setItemToLs("userName", user_name);
+                setItemToLs("userEmail", user_email);
                 alert("카카오로 로그인 완료!");
-                navigate("/");
+                navigate("/main");
               });
           });
       })
--- src/elements/introMain/Oauth.js
+++ src/elements/introMain/Oauth.js
@@ -1,2 +1,5 @@
+const KAKAO_CALLBACK_URL = "http://localhost:3000/oauth/kakao/callback";
+const KAKAO_KEY = "4689e9fb2dad975cc84333a495fc2238";
+
 // export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.REACT_APP_KEY_NAVER}&state=teamnote&redirect_uri=${process.env.REACT_APP_URI_NAVER}`;
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KEY_KAKAO}&redirect_uri=${process.env.REACT_APP_URI_KAKAO}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_KEY}&redirect_uri=${KAKAO_CALLBACK_URL}&response_type=code`;
